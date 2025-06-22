dae45e8aa7d7fd8794605c8f57d751186f9b015a
MartinEesmaa <martin.eesmaa@protonmail.com>
2025-06-22 22:24:16 +1000

    Replace latest by date tag of GCC toolchain

--- toolchain/gcc/gcc.cmake
+++ toolchain/gcc/gcc.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(gcc
     DEPENDS
         mingw-w64-headers
-    URL https://www.mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/LATEST-14/gcc-14-20250620.tar.xz
+    URL https://www.mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/14-20250620/gcc-14-20250620.tar.xz
     # https://mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/13-20240309/sha512.sum
     URL_HASH SHA512=e5a99c1fcac867787d2e96b1c0f9180813a7cdc7ccb5f4002644c619edb0296d4a227ef62e988a98528b7430c35e69bc6e46981b90db5f599d1045ef96a9669c
     DOWNLOAD_DIR ${SOURCE_LOCATION}
